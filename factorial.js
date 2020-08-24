d27b970fd4edc1a7712e30e95686e23804ccc0f7
ydvorzhetskiy <ydvorzhetskiy@navicons.ru>
2020-08-24 20:29:56 +0300

    1

--- factorial.js
+++ factorial.js
@@ -9,3 +9,13 @@ for (const value of factorial()) {
   console.log(value);
   if (value > 2000) break;
 }
+
+function* alphabet() {
+  for (let c = 'a'; c <= 'z'; c = String.fromCharCode(c.charCodeAt(0) + 1)) {
+    yield c;
+  }
+}
+
+for (const value of alphabet()) {
+  console.log(value);
+}
